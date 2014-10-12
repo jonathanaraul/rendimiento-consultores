f71e71b6241b40ec76e51dd6fe7e939fea9b8179
cloker19 <cloker19@gmail.com>
2014-10-12 01:31:58 -0430

    Creacion de vista relatorio.blade.php

---
+++ app/views/site/data/relatorio.blade.php
@@ -0,0 +1,87 @@
+<!-- BEGIN SAMPLE TABLE PORTLET-->
+<div class="portlet box blue">
+	<div class="portlet-title">
+		<div class="caption">
+			<i class="fa fa-cogs"></i>Nombre de Usuario
+		</div>
+	</div>
+	<div class="portlet-body">
+		<div class="table-responsive">
+			<table class="table table-bordered">
+			<thead>
+			<tr>
+				<th>
+					 Período
+				</th>
+				<th>
+					 Receita Líquida
+				</th>
+				<th>
+					 Custo Fixo
+				</th>
+				<th>
+					 Comissão
+				</th>
+				<th>
+					 Lucro
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td>
+					 Janeiro de 2007
+				</td>
+				<td>
+					 R$ 1.500,00
+				</td>
+				<td>
+					 - R$ 2.000,00
+				</td>
+				<td>
+					 - R$ 1.000,00
+				</td>
+				<td>
+					 - R$ 1.500,00
+				</td>
+			</tr>
+			<tr>
+				<td>
+					 Fevereiro de 2007
+				</td>
+				<td>
+					 R$ 25.000,00
+				</td>
+				<td>
+					 - R$ 2.000,00
+				</td>
+				<td>
+					 - R$ 2.500,00
+				</td>
+				<td>
+					 R$ 20.500,00
+				</td>
+			</tr>
+			<tr>
+				<td>
+					 SALDO
+				</td>
+				<td>
+					 R$ 26.500,00
+				</td>
+				<td>
+					 - R$ 4.000,00
+				</td>
+				<td>
+					 - R$ 3.500,00
+				</td>
+				<td>
+					 R$ 19.000,00
+				</td>
+			</tr>
+			</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<!-- END SAMPLE TABLE PORTLET-->
